77c0150500607565e4cc29438c3ca7070bbfaa35
santhan <santhankrishna18@gmail.com>
2024-02-10 00:07:50 +0530

    all

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { KComponent } from './k/k.component';
+import { GeneralComponent } from './general/general.component';
+import { JointComponent } from './joint/joint.component';
+import { BrokenbonesComponent } from './brokenbones/brokenbones.component';
+import { SnakeBiteComponent } from './snake-bite/snake-bite.component';
+import { PComponent } from './p/p.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+export const routes: Routes = [
+    {path:'k',component:KComponent},
+    {path:'general',component:GeneralComponent},
+    {path:'joint',component:JointComponent},
+    {path:'brokenBones',component:BrokenbonesComponent},
+    {path:'snakeBite',component:SnakeBiteComponent},
+    {path:'p',component:PComponent},
+    {path:'home',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+];
---
+++ src/app/k/k.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-k',
+  standalone: true,
+  imports: [],
+  templateUrl: './k.component.html',
+  styleUrl: './k.component.css'
+})
+export class KComponent {
+
+}
